cf44c154d602029f6dcb1bee64d445e895ae9430
jerimkaura <jerimkaura001@gmail.com>
2022-12-07 19:53:24 +0300

    - minor

---
+++ common/src/main/java/com/cookpad/common/util/Resource.kt
@@ -0,0 +1,7 @@
+package com.cookpad.common.util
+
+sealed class Resource<T>(val data: T? = null, val error: String? = null) {
+    class Success<T>(data: T): Resource<T>(data)
+    class Loading<T> (data: T? = null): Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+}
